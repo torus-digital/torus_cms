fe19e87b08009bcab2f949a1d161fb964f67041c
gkpty <gkardonski@gmail.com>
2019-02-09 06:08:17 -0500

    added editor

---
+++ almost_cms/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import './App.css';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+class EditorConvertToHTML extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  }
+
+  onEditorStateChange: Function = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div className="App">
+        <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditorConvertToHTML;
