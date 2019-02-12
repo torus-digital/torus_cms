df5fb4b56744f4df0ffcfad94a9c27a16135bc10
gkpty <gkardonski@gmail.com>
2019-02-12 02:47:05 -0500

    added amplify and functionality to upload articles and images into s3 bucket

--- almost_cms/src/index.js
+++ almost_cms/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
