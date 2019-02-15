8290c6f1e6624986e02465d10375929359805b1b
gkpty <gkardonski@gmail.com>
2019-02-14 21:38:46 -0500

    changed gallery title

--- almost_cms/src/App.js
+++ almost_cms/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import { Storage } from 'aws-amplify'
-import Amplify, { graphqlOperation }  from "aws-amplify";
+import { graphqlOperation }  from "aws-amplify";
 import { Connect } from "aws-amplify-react";
 import * as mutations from './graphql/mutations';
 
@@ -72,17 +72,66 @@ class AddArticle extends Component {
   }
 }
 
-class App extends Component {
-
-
+class AddImage extends Component {
   onChange(e) {
-      const file = e.target.files[0];
-      Storage.put('example.png', file, {
-          contentType: 'image/png'
-      })
-      .then (result => console.log(result))
-      .catch(err => console.log(err));
+    const file = e.target.files[0];
+    Storage.put(this.state.name, file, {
+        contentType: 'image/'
+    })
+    .then (result => console.log(result))
+    .catch(err => console.log(err));
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: '',
+        file: null,
+    };
+  }
+
+  handleChange(name, ev) {
+      this.setState({ [name]: ev.target.value });
+  }
+
+  async submit() {
+    const { onCreate } = this.props;
+    var input = {
+      name: this.state.name,
+      file: this.state.file,
+    }
+    console.log(input);
+    await onCreate({input})
+  }
+
+  render(){
+    return (
+        <div>
+          <div className="container section">
+          <h1>Post an Image</h1>
+          <input
+            name="name"
+            placeholder="name"
+            onChange={(ev) => { this.handleChange('name', ev)}}
+          />
+          <input
+            name="file" 
+            placeholder="file"
+            type="file" 
+            accept='image/'
+            onChange={(e) => this.onChange(e)}
+          />
+          <button onClick={this.submit.bind(this)}>
+            Add
+          </button>
+        </div>
+     
+      </div>
+    );
+  }
+}
+
+class App extends Component {
 
   render() {
     return (
@@ -93,16 +142,16 @@ class App extends Component {
         )}
       </Connect>
 
+      <Connect mutation={graphqlOperation(mutations.createImage)}>
+        {({mutation}) => (
+          <AddImage onCreate={mutation} />
+        )}
+      </Connect>
+
       <div className="App">
         
       </div>
-      <div className="container section">
-        <h1>Post a picture</h1>
-        <input
-          type="file" accept='image/png'
-          onChange={(e) => this.onChange(e)}
-        />
-      </div>
+      
     </>
 
     );
