d556e14a622e151fe644d283bcad286e33680bef
gkpty <gkardonski@gmail.com>
2019-02-18 04:36:59 -0500

    got the image and article mutations working

--- almost_cms/src/App.js
+++ almost_cms/src/App.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+//rich text editor
 import { EditorState, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 //import htmlToDraft from 'html-to-draftjs';
-import './App.css';
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import { Storage } from 'aws-amplify'
+
+//article mutations
 import { graphqlOperation }  from "aws-amplify";
 import { Connect } from "aws-amplify-react";
-import * as mutations from './graphql/mutations';
+import * as mutations from './GraphQL/MutationCreateArticle';
+
+//AppSync and Apollo libraries
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
+
+//Amplify
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+
+// Components
+import AllPhotos from "./Components/AllPhotos";
+import AddPhoto from "./Components/AddPhoto";
+
+import awsconfig from './aws-exports';
+
+// Amplify init
+Amplify.configure(awsconfig);
+
+const GRAPHQL_API_REGION = awsconfig.aws_appsync_region
+const GRAPHQL_API_ENDPOINT_URL = awsconfig.aws_appsync_graphqlEndpoint
+const S3_BUCKET_REGION = awsconfig.aws_user_files_s3_bucket_region
+const S3_BUCKET_NAME = awsconfig.aws_user_files_s3_bucket
+const AUTH_TYPE = awsconfig.aws_appsync_authenticationType
+
+// AppSync client instantiation
+const client = new AWSAppSyncClient({
+  url: GRAPHQL_API_ENDPOINT_URL,
+  region: GRAPHQL_API_REGION,
+  auth: {
+    type: AUTH_TYPE,
+    // Get the currently logged in users credential.
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+  },
+  // Amplify uses Amazon IAM to authorize calls to Amazon S3. This provides the relevant IAM credentials.
+  complexObjectsCredentials: () => Auth.currentCredentials()
+});
 
 class AddArticle extends Component {
     state = {
@@ -72,91 +113,40 @@ class AddArticle extends Component {
   }
 }
 
-class AddImage extends Component {
-  onChange(e) {
-    const file = e.target.files[0];
-    Storage.put(this.state.name, file, {
-        contentType: 'image/'
-    })
-    .then (result => console.log(result))
-    .catch(err => console.log(err));
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        name: '',
-        file: null,
-    };
-  }
-
-  handleChange(name, ev) {
-      this.setState({ [name]: ev.target.value });
-  }
-
-  async submit() {
-    const { onCreate } = this.props;
-    var input = {
-      name: this.state.name,
-      file: this.state.file,
-    }
-    console.log(input);
-    await onCreate({input})
-  }
-
-  render(){
-    return (
-        <div>
-          <div className="container section">
-          <h1>Post an Image</h1>
-          <input
-            name="name"
-            placeholder="name"
-            onChange={(ev) => { this.handleChange('name', ev)}}
-          />
-          <input
-            name="file" 
-            placeholder="file"
-            type="file" 
-            accept='image/'
-            onChange={(e) => this.onChange(e)}
-          />
-          <button onClick={this.submit.bind(this)}>
-            Add
-          </button>
-        </div>
-     
-      </div>
-    );
-  }
-}
-
 class App extends Component {
 
   render() {
     return (
-    <>
-      <Connect mutation={graphqlOperation(mutations.createArticle)}>
-        {({mutation}) => (
-          <AddArticle onCreate={mutation} />
-        )}
-      </Connect>
-
-      <Connect mutation={graphqlOperation(mutations.createImage)}>
-        {({mutation}) => (
-          <AddImage onCreate={mutation} />
-        )}
-      </Connect>
-
       <div className="App">
+        <div>
+          <Connect mutation={graphqlOperation(mutations.createArticle)}>
+            {({mutation}) => (
+              <AddArticle onCreate={mutation} />
+            )}
+          </Connect>
+        </div>
+        <div>
+          <div>
+            <h1>Post a Picture</h1>
+          </div>
+          <div className="App-content">
+            <AddPhoto options={{ bucket: S3_BUCKET_NAME, region: S3_BUCKET_REGION }} />
+            <AllPhotos />
+          </div>
+        </div>
+
         
       </div>
-      
-    </>
-
     );
   }
 }
 
+const AppWithAuth = withAuthenticator(App, true);
 
-export default withAuthenticator(App, { includeGreetings: true })
+export default () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <AppWithAuth />
+    </Rehydrated>
+  </ApolloProvider>
+);
