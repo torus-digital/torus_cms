a114a87702a485f449cbb4844122297c6c16f367
gkpty <gkardonski@gmail.com>
2019-02-19 01:57:27 -0500

    changed a lil bit more stuff

---
+++ copyFunction.js
@@ -0,0 +1,31 @@
+// Load the AWS SDK
+const aws = require('aws-sdk');
+
+// Construct the AWS S3 Object - http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+const s3 = new aws.S3({
+    apiVersion: '2006-03-01'
+ });
+        
+// Define 2 new variables for the source and destination buckets
+var srcBucket = "***REMOVED***";
+var destBucket = "***REMOVED***";
+var exTension = "private/us-east-1:ac06372e-5966-482a-b1eb-df3e87441368"
+var sourceObject = "89573202-bb97-4b2d-b93c-18cb9959f0d6.jpg";
+
+//Main function
+exports.handler = (event, context, callback) => {
+        
+//Copy the current object to the destination bucket - http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#copyObject-property
+s3.copyObject({ 
+    CopySource: srcBucket + '/' + exTension + '/' + sourceObject,
+    Bucket: destBucket,
+    Key: sourceObject
+    }, function(copyErr, copyData){
+       if (copyErr) {
+            console.log("Error: " + copyErr);
+         } else {
+            console.log('Copied OK');
+         } 
+    });
+  callback(null, 'All done!');
+};
