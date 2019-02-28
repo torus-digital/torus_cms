da16e866c6d425e3ebfea56488d7df8dd9cbf7f8
gkpty <gkardonski@gmail.com>
2019-02-28 02:27:48 -0500

    added new class to create article

--- almost_cms/src/App.js
+++ almost_cms/src/App.js
@@ -52,6 +52,15 @@ const client = new AWSAppSyncClient({
   complexObjectsCredentials: () => Auth.currentCredentials()
 });
 
+
+class CreateArticle extends Component {
+  heading = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <meta name="description" content=""> <meta name="author" content=""> <title>Creative - Start Bootstrap Theme</title> <!-- Font Awesome Icons --> <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> <!-- Google Fonts --> <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet"> <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css"> <!-- Plugin CSS --> <link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> <!-- Theme CSS - Includes Bootstrap --> <link href="../css/index.css" rel="stylesheet"> </head> <body id="page-top"></body>';
+  nav = '';
+  header = '';
+  body = '';
+  footer = '';
+}
+
 class AddArticle extends Component {
     state = {
     editorState: EditorState.createEmpty(),
