db5f2168b876fd7002edcc33f99764f511ec364c
gkpty <gkardonski@gmail.com>
2019-03-17 18:56:56 -0500

    refactored components and solved scope. still having issues with async promise statement from addToStorage

--- almost_cms/src/Components/Shared.js
+++ almost_cms/src/Components/Shared.js
@@ -1,14 +1,32 @@
-import React from 'react';
 import axios from 'axios';
+import { Storage } from 'aws-amplify'
 
-function CopyToBucket(instructions) {
+
+export function CopyToBucket(bucketVars) {
     var x = '';
-    const url = `https://jsonplaceholder.typicode.com/users`
-    axios.post( url, { instructions })
+    const url = `https://dquw9oidca.execute-api.us-east-1.amazonaws.com/postCopyFunctionInitial/`;
+
+    const headers = {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/x-www-form-urlencoded' 
+    };
+
+    axios.post( url, bucketVars, { headers } )
       .then(res => {
         console.log(res);
         console.log(res.data);
-        x = res.data
+        x = res.data;
       })
     return x;
+}
+
+export function addToStorage(contentType, section, title, fileObj) {
+    let ax = Storage.put(`${section}/${title}.html`, fileObj, {contentType: contentType})
+    .then (result => {
+        console.log('result: ', result);
+        console.log(result);
+        return result;
+    })
+    .catch(err => console.log('error: ', err));
+    return ax;
 }
