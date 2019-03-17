1bdc60d66a3f246c0ddf4ad23501d34909886ac7
gkpty <gkardonski@gmail.com>
2019-03-17 18:56:56 -0500

    refactored components and solved scope. still having issues with async promise statement from addToStorage

--- almost_cms/src/App.js
+++ almost_cms/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
 import './App.css';
+import './Components/AddArticle.js';
 import 'semantic-ui-css/semantic.min.css'
 
 //rich text editor
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import draftToHtml from 'draftjs-to-html';
-//import htmlToDraft from 'html-to-draftjs';
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 //article mutations
@@ -23,11 +19,11 @@ import { ApolloProvider } from 'react-apollo';
 //Amplify
 import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
-import { Storage } from 'aws-amplify'
 
 // Components
 import AllPhotos from "./Components/AllPhotos";
 import AddPhoto from "./Components/AddPhoto";
+import AddArticle from "./Components/AddArticle"
 
 import awsconfig from './aws-exports';
 
@@ -52,85 +48,6 @@ const client = new AWSAppSyncClient({
   // Amplify uses Amazon IAM to authorize calls to Amazon S3. This provides the relevant IAM credentials.
   complexObjectsCredentials: () => Auth.currentCredentials()
 });
-
-class AddArticle extends Component {
-    state = {
-    editorState: EditorState.createEmpty(),
-  }
-
-  onEditorStateChange: Function = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        title: '',
-        body: '',
-    };
-  }
-
-  handleChange(title, ev) {
-      this.setState({ [title]: ev.target.value });
-  }
-
-  async submit() {
-    const { editorState } = this.state;
-    const { onCreate } = this.props;
-    var input = {
-      title: this.state.title,
-      body: draftToHtml(convertToRaw(editorState.getCurrentContent())),
-    }
-    console.log(input);
-    await onCreate({input})
-  }
-
-  addToStorage = () => {
-    const { editorState } = this.state;
-    const heading = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <meta name="description" content=""> <meta name="author" content=""> <title>Creative - Start Bootstrap Theme</title> <!-- Font Awesome Icons --> <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> <!-- Google Fonts --> <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet"> <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css"> <!-- Plugin CSS --> <link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> <!-- Theme CSS - Includes Bootstrap --> <link href="../css/index.css" rel="stylesheet"> </head> <body id="page-top">';
-    const nav = '<nav class="navbar navbar-expand-lg fixed-top py-3" id="mainNav"> <div class="container"> <a class="navbar-brand js-scroll-trigger" href="../index.html">Almost CMS</a> <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navbarResponsive"> <ul class="navbar-nav ml-auto my-2 my-lg-0"> <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="../about.html">About</a> </li> <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="index.html">Articles</a> </li> <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="../images/gallery/index.html">Gallery</a> </li> <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="../contact.html">Contact</a> </li> </ul> </div> </div> </nav>';
-    const header = '<section class="page-heading bg-primary" id="about"> <div class="container"> <div class="row justify-content-center"> <div class="col-lg-8 text-center"> <h4 class="text-white mt-0">The latest on Almost CMS</h4> </div> </div> </div> </section>';
-    const footer = '<footer class="bg-light py-5"> <div class="container-fluid"> <div class="row small text-center text-muted"> <div class="col-md-6"> <div>Copyright &copy; 2019 - Almost CMS</div> </div> <div class="col-md-6"> <div> Powered By <a href="http://***REMOVED***">Almost CMS</a></div> </div> </div> </div> </footer>';
-    var body = draftToHtml(convertToRaw(editorState.getCurrentContent()));
-    var page = heading + nav + header + body + footer;
-    Storage.put(`articles/${this.state.title}.html`, page, {contentType: 'text/html'})
-    .then (result => {
-      console.log('result: ', result)
-    })
-    .catch(err => console.log('error: ', err));
-  }
-
-  render(){
-    const { editorState } = this.state;
-    return (
-        <div>
-          <div className="container">
-          <h1>Post a comment</h1>
-          <input
-                name="title"
-                placeholder="title"
-                onChange={(ev) => { this.handleChange('title', ev)}}
-          />
-          <Editor
-            editorState={editorState}
-            wrapperClassName="demo-wrapper"
-            editorClassName="demo-editor"
-            onEditorStateChange={this.onEditorStateChange}
-          />
-          <button onClick={this.submit.bind(this)}>
-            Add to DB
-          </button>
-        </div>
-        <div>
-          <button onClick={this.addToStorage}>Create Article</button>
-        </div>
-     
-      </div>
-    );
-  }
-}
 
 class App extends Component {
 
