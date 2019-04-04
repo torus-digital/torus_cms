2fa5b52e3727669089fdc1f3b1bb4f98facf815c
gkpty <gkardonski@gmail.com>
2019-04-04 04:23:42 -0500

    added disable button article button and fixed console log messages

--- almost_cms/src/Components/AddPicture.js
+++ almost_cms/src/Components/AddPicture.js
@@ -44,47 +44,48 @@ class AddPicture extends Component {
       switch(addPicture) {
         // if the create index function is succesful
         case 'Success':
-          console.log(`Succesfully added your image ${file_str} to your s3 storage bucket.`)
           const bucketVars = {
             sourceRoute: `public/${section}`,
             sourceObject: `${this.state.title}.${ext}`,
             destRoute: `${section}`
           };
-          console.log("bucketvars: ", bucketVars);
           let copyPicture = await new copyToBucket(bucketVars);
-          console.log(copyPicture)
-            switch(copyPicture) {
-              // If the copy article function is succesful
-              case 'Success':
-                console.log("Succesfully copied the new picture to your public bucket!");
-                // execute the create index function
-                let addGallery = await new createGallery();
-                switch(addGallery) {
-                  // if the create index function is succesful
-                  case 'Success':
-                    console.log("Successfully created a gallery with your new picture and added it to your S3 storage bucket!");
-                    // execute the copy index function
-                    const bucketVars = {
-                      sourceRoute: 'public/gallery',
-                      sourceObject: 'gallery.html',
-                      destRoute: 'gallery'
-                    };
-                    let copyGallery = copyToBucket(bucketVars);
-                    console.log("index copy receipt: ", copyGallery);
-                    console.log( "Congratulations! you have succesfully posted your picture!" );
-                    break;
-                  default:
-                    console.log("Error: Failed to save the new picture gallery");
-                }
-                break;
-              default:
-                console.log(copyPicture)
-                console.log("Error: Failed to copy your picture to your public bucket.");
-            }
-            break;
-          default:
-            console.log("Error: Failed to add your picture to storage."); 
-        }
+          switch(copyPicture) {
+            // If the copy article function is succesful
+            case 'Success':
+              console.log(`Succesfully copied ${bucketVars.sourceObject} to ${bucketVars.destRoute}`);
+              // execute the create index function
+              let addGallery = await new createGallery();
+              switch(addGallery) {
+                // if the create index function is succesful
+                case 'Success':
+                  // execute the copy index function
+                  const bucketVars = {
+                    sourceRoute: 'public/gallery',
+                    sourceObject: 'index.html',
+                    destRoute: 'gallery'
+                  };
+                  let copyGallery = await new copyToBucket(bucketVars);
+                  switch(copyGallery) {
+                    case 'Success':
+                      console.log( "Congratulations! you have succesfully published your new picture!" );
+                      break;
+                    default:
+                      console.log("Error: Failed to copy your gallery")
+                  }
+                  break;
+                default:
+                  console.log("Error: Failed to save the new picture gallery");
+              }
+              break;
+            default:
+              console.log(copyPicture)
+              console.log("Error: Failed to copy your picture to your public bucket.");
+          }
+          break;
+        default:
+          console.log("Error: Failed to add your picture to storage."); 
+      }
     }
   
     render(){
