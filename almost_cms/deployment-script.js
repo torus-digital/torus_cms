e40414664db6cb39e4fd0a12b95bc0bf9c0b22ab
gkpty <gkardonski@gmail.com>
2019-04-14 16:43:48 -0500

    adds the alias A record for www

--- almost_cms/deployment-script.js
+++ almost_cms/deployment-script.js
@@ -9,6 +9,7 @@ var AWS = require('aws-sdk');
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 route53 = new AWS.Route53({apiVersion: '2013-04-01'});
+var wname = 'www.' + process.argv[2];
 
 // CREATE THE ROOT S3 BUCKET
 
@@ -45,7 +46,6 @@ s3.createBucket(bucketParams, function(err, data) {
 				// CREATE THE WWW REROUTE BUCKET
 
 				// Create params JSON for S3.createBucket
-				var wname = 'www.' + process.argv[2];
 				var wbucketParams = {
 					Bucket : wname,
 				};
@@ -85,7 +85,7 @@ s3.createBucket(bucketParams, function(err, data) {
 // Create Route53 Hosted Zone
 var call_ref = new Date().toString();
 
-var params = {
+var hostZoneParams = {
 	CallerReference: call_ref, /* required */
 	Name: process.argv[2], /* required */
 	HostedZoneConfig: {
@@ -93,7 +93,7 @@ var params = {
 		PrivateZone: false
 	}
 };
-route53.createHostedZone(params, function(err, data) {
+route53.createHostedZone(hostZoneParams, function(err, data) {
 	if (err) {
 		console.log(err, err.stack); // an error occurred
 	} 
@@ -102,7 +102,7 @@ route53.createHostedZone(params, function(err, data) {
 		const hosted_id = data.HostedZone.Id.slice(data.HostedZone.Id.lastIndexOf('/') + 1);
 
 		// Create the Alias A record for the root bucket
-		var params = {
+		var aliasParams = {
 			ChangeBatch: {
 				 Changes: [{
 					Action: "CREATE", 
@@ -120,7 +120,7 @@ route53.createHostedZone(params, function(err, data) {
 			}, 
 			HostedZoneId: hosted_id // the Id of your recently created hosted zone
 		 };
-		route53.changeResourceRecordSets(params, function(err, data) {
+		route53.changeResourceRecordSets(aliasParams, function(err, data) {
 			if (err) {
 				console.log(err, err.stack); // an error occurred
 			} 
@@ -130,6 +130,32 @@ route53.createHostedZone(params, function(err, data) {
 		});
 
 		//Create the Alias A record for the www bucket
+		var waliasParams = {
+			ChangeBatch: {
+				 Changes: [{
+					Action: "CREATE", 
+					ResourceRecordSet: {
+						AliasTarget: {
+							DNSName: "s3-website-us-east-1.amazonaws.com", 
+							EvaluateTargetHealth: false, 
+							HostedZoneId: 'Z3AQBSTGFYJSTF' // a code depending on your region and resource for more info refer to https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_website_region_endpoints
+						}, 
+						Name: wname, 
+						Type: "A"
+					 }
+				}], 
+				Comment: "Alias Record for S3 Bucket"
+			}, 
+			HostedZoneId: hosted_id // the Id of your recently created hosted zone
+		 };
+		route53.changeResourceRecordSets(waliasParams, function(err, data) {
+			if (err) {
+				console.log(err, err.stack); // an error occurred
+			} 
+			else {
+				console.log(data); // successful response
+			}
+		});
 
 	}
 });
