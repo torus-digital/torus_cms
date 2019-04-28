3ae3d6d80c96c1fbe0c4bf934416e53eeb437fcd
gkpty <gkardonski@gmail.com>
2019-04-28 15:39:24 -0500

    finally got the promise to load on the select list on load

--- almost_cms/src/Components/AddArticle.js
+++ almost_cms/src/Components/AddArticle.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../App.css';
 import 'semantic-ui-css/semantic.min.css';
 import createArticle from './CreateArticle';
+import { API, graphqlOperation } from "aws-amplify";
+import gql from '../GraphQL/QueryListArticles';
 
 //rich text editor
 import { EditorState, convertToRaw } from 'draft-js';
@@ -25,10 +27,28 @@ class AddArticle extends Component {
 			title: '',
 			body_html: '',
 			body_txt: '',
+			list: [],
 		};
 		this.handleChange = this.handleChange.bind(this)
 		this.handleSubmit = this.handleSubmit.bind(this)
 	}
+
+	componentDidMount() {
+    let initialList = [];
+    API.graphql(graphqlOperation(gql))
+        .then(result => {
+            return result.data.listArticles.items;
+        }).then(data => {
+        initialList = data.map((article) => {
+            return article
+        });
+        console.log(initialList);
+        this.setState({
+            list: initialList,
+        });
+    });
+	}
+
 
 	state = {
     editorState: EditorState.createEmpty(),
@@ -37,7 +57,7 @@ class AddArticle extends Component {
     this.setState({
       editorState,
     });
-  };
+	};
   
 	handleChange(event) {
 		this.setState({ [event.target.name]: event.target.value })
@@ -54,11 +74,19 @@ class AddArticle extends Component {
 		submitArticle(txt_body, input)
 		event.preventDefault()    
 	}
-	  
+	
 	render() {
+		let list = this.state.list;
+		console.log(list)
+		let optionItems = list.map((article, i) =>
+			<option key={i} value={article.id}>{article.title}</option>
+		);
 		const { editorState } = this.state;
 		return (
 			<div>
+				<select>
+					{optionItems}
+				</select>
 				<div className="container">
 					<h1>Post a comment</h1>
 					<form onSubmit={this.handleSubmit}>
